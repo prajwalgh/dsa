0dfdd5716d09691c7d524e368ad0801f501c01fa
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2022-09-13 23:39:41 +0530

    LL mid node   13-09-22

--- heap/maxEffiOfTeam.py
+++ heap/maxEffiOfTeam.py
@@ -14,6 +14,9 @@
 # Output: 60
 # Explanation:
 # We have the maximum performance of the team by selecting engineer 2 (with speed=10 and efficiency=4) and engineer 5 (with speed=5 and efficiency=7). That is, performance = (10 + 5) * min(4, 7) = 60.
+import heapq
+
+
 class Solution(object):
     def maxPerformance(self, n, speed, efficiency, k):
         eng = []
