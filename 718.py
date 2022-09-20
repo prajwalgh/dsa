bbdbf6b8a710635e00baf50c283b5fc40ff0321e
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2022-09-20 12:36:39 +0530

    718 little change 20-09-22

--- 718.py
+++ 718.py
@@ -2,15 +2,15 @@ class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         strnum2 = ''.join([chr(x) for x in nums2])
         print(strnum2)
-         strmax = ''
-          ans = 0
-           for num in nums1:
-                strmax += chr(num)
-                if strmax in strnum2:
-                    ans = max(ans, len(strmax))
-                else:
-                    strmax = strmax[1:]
-            return ans
+        strmax = ''
+        ans = 0
+        for num in nums1:
+            strmax += chr(num)
+            if strmax in strnum2:
+                ans = max(ans, len(strmax))
+            else:
+                strmax = strmax[1:]
+        return ans
 # on liner
         # class Solution:
 #     def findLength(self, n1: List[int], n2: List[int]) -> int:
