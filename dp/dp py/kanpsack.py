73c6400623d4e13615e967212cbcd3c916a876d7
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2023-02-10 16:56:03 +0530

    knapsack memo approach

--- dp/dp py/kanpsack.py
+++ dp/dp py/kanpsack.py
@@ -3,13 +3,15 @@ wt = [10, 20, 30]
 W = 50
 n = len(val)
 
+# this is recursice approach              error : Time Limit Exceeded
+
 
 def knapSack(W, wt, val, n):
-    # print("ddd")
+    # base case
     if W == 0 or n == 0:
         return 0
+    # main body
     if wt[n - 1] <= W:
-        # print(val[n-1])
         return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1))
     elif wt[n - 1] > W:
         # print(val[n-1])
@@ -18,11 +20,37 @@ def knapSack(W, wt, val, n):
 
 print(knapSack(W, wt, val, n))
 
+# memoization approach -----------------------------------
+
+# method 1
+
+
+class Solution:
+
+    # Function to return max value that can be put in knapsack of capacity W.
+
+    def __init__(self):
+        self.memo = [[-1 for i in range(1001)] for j in range(1001)]
+
+    def knapSack(self, W, wt, val, n):
+        if n == 0 or W == 0:
+            return 0
+        if self.memo[n][W] != -1:
+            return self.memo[n][W]
+        if wt[n-1] <= W:
+            self.memo[n][W] = max(
+                val[n-1]+self.knapSack(W-wt[n-1], wt, val, n-1), self.knapSack(W, wt, val, n-1))
+            return self.memo[n][W]
+        elif wt[n-1] > W:
+            self.memo[n][W] = self.knapSack(W, wt, val, n-1)
+            return self.memo[n][W]
 
 # A Dynamic Programming based Python
 # Program for 0-1 Knapsack problem
 # Returns the maximum value that can
 # be put in a knapsack of capacity W
+
+# method 2
 
 
 def knapSack_memo(W, wt, val, n):
@@ -46,3 +74,4 @@ def knapSack_memo(W, wt, val, n):
 print(knapSack_memo(W, wt, val, n))
 
 # This code is contributed by Bhavya Jain
+# memoization approach -----------------------------------
