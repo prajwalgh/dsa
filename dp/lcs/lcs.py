1acffbc203e1672e434472775c2157af0b849ea6
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2023-03-25 15:58:53 +0530

    lcs top down

--- dp/lcs/lcs.py
+++ dp/lcs/lcs.py
@@ -38,3 +38,18 @@ class Solution:
                                 self.lcs(x, y-1, s1, s2))
         return self.dp[x][y]
         # code here
+# methiod 3 :top down optimal
+
+
+class Solution:
+    # Function to find the length of longest common subsequence in two strings.
+    def lcs(self, x, y, s1, s2):
+        dp = [[0 for i in range(y+1)]for j in range(x+1)]
+        for i in range(1, x+1):
+            for j in range(1, y+1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[x][y]
