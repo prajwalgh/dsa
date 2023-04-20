5b3052a5b2bf3f01a265b42d1e598037abc8c631
prajwalgh <76061744+prajwalgh@users.noreply.github.com>
2023-04-20 14:03:42 +0530

    662

---
+++ 1662.check-if-two-string-arrays-are-equivalent.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1662 lang=python
+#
+# [1662] Check If Two String Arrays are Equivalent
+#
+
+# @lc code=start
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+        
+# @lc code=end
+
---
+++ 662.maximum-width-of-binary-tree.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=662 lang=python
+#
+# [662] Maximum Width of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def levl(root):
+            q = [(root, 0)]
+            ans = 0
+            while len(q) != 0:
+                len1 = len(q)
+                start = q[0][1]
+                end = q[-1][1]
+                ans = max(ans, end-start+1)
+                for i in range(len1):
+                    node, index = q.pop(0)
+                    if node.left:
+                        q.append((node.left, index*2+1))
+                    if node.right:
+                        q.append((node.right, index*2+2))
+            return ans
+        return levl(root)
+
+# @lc code=end
